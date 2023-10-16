4d43f4e9c077c30be5fea869e61f610aacebdf42
rakeshboyiri <boyirirakesh@gmail.com>
2023-10-17 00:50:55 +0530

    SingleDimensionalArray

---
+++ java-practice/SingleDimensionalArray.java
@@ -0,0 +1,12 @@
+public class SingleDimensionalArray {
+    public static void main(String[] args){
+
+       // int arr[] = new int[5];
+       int arr[] = {10,11,12,13,14,15};
+       int i;
+       for(i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+       }     
+
+    }
+}
