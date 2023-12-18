b133b65e77c7b6cfc32fadb41d7e71d7823ee63a
rakeshboyiri <boyirirakesh@gmail.com>
2023-12-18 19:13:47 +0530

    java

---
+++ exercise/week_8/First.java
@@ -0,0 +1,9 @@
+//package FirstPackage;
+
+public class First {
+    public void  display(){
+        System.out.println("Im First class");
+    }
+    public static void main(String args[]){
+    }
+}
