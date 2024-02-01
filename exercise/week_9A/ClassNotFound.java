5f06d2c0e2934af5d04994c56ae49a5a64a403e4
rakeshboyiri <boyirirakesh@gmail.com>
2024-02-01 21:50:40 +0530

    java

--- exercise/week_9A/ClassNotFound.java
+++ exercise/week_9A/ClassNotFound.java
@@ -1,16 +1,26 @@
+class a
+{
+}
 public class ClassNotFound {
     public static void main(String[] args) {
+       
+       
+       
         try {
-            // Attempt to load a class that may not exist
+     
+           //  Attempt to load a class that may not exist
+           a obj =new a();
             Class<?> dynamicClass = Class.forName("NonExistentClass");
 
-            // If the class is found, print its name
+             //If the class is found, print its name
             System.out.println("Class found: " + dynamicClass.getName());
-        } catch (ClassNotFoundException e) {
+       } catch (Exception e) {
             // Handle ClassNotFoundException
             System.err.println("Class not found: " + e.getMessage());
+          
             
         }
+        
         
     }
 }
--- exercise/week_9A/n.py
+++
@@ -1,1 +0,0 @@
-select sname from salesperson where sid in(select distinct sid from sales where pid in (select pid from products where pname="maggie") and pid in (select pid from products where pname="good day"));
