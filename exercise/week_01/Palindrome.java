8ce45e3d4a3991e242145482cab805441b962243
rakeshboyiri <boyirirakesh@gmail.com>
2024-04-03 07:27:35 +0530

    java

---
+++ exercise/week_01/Palindrome.java
@@ -0,0 +1,26 @@
+/*
+ * 5. Write a java program to find whether given number is Palindrome or not
+ */
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args){
+        int n , org ;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        n = org = sc.nextInt();
+        int rev =0 , rem ;
+        while(n>0){
+            rem = n%10;
+            rev = rev * 10 + rem ;
+            n = n /10 ;
+        }   
+        if(rev == org){
+            System.out.println(org +" is Palindrome");
+        }
+        else{
+            System.out.println(org +" is not Palindrome");
+        }
+
+        sc.close();
+    }
+}
